4381b3fb72206233c1a38780f9601582c9b0ebe1
Ayush Jhunjhunwala <ayushjhun13@gmail.com>
2024-05-07 15:47:11 +0530

    About Page done for now.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,15 +1,79 @@
-<div>
-  <p class="font-lato">
-    Making the right IT choices can be difficult. That's wherewe come in. Prime
-    Technologies has the expertise and skill-set necessary to help you make IT
-    choices that are practical, efficient & reliable when it matters the most.
-    This text should be Lato. SEE OUR PROJECTS
-  </p>
+<script>
+  import aboutImage from "../../lib/images/AboutImage.svg";
+</script>
 
+<div class="text-center flex justify-center">
   <p
-    on:load={() =>
-      console.log(document.defaultView.getComputedStyle(this).fontFamily)}
+    class="homeHeading font-semibold font-Montserrat w-fit xl:text-7xl lg:text-6xl md:text-5xl sm:text-5xl xs:text-4xl xxs:text-3xl"
   >
-    This text should be Lato (if loaded).
+    ABOUT <span class="text-[#6F42C1]">PRIME</span> TECHNOLOGY
   </p>
 </div>
+<div class="flex aboutContainer">
+  <div class="w-1/2 p-16 flex flex-col align-center justify-center aboutText">
+    <!-- <h1 class="font-bold text-2xl mb-2">About Prime Technology:</h1> -->
+    <p>
+      Prime Technology delivers expert solutions in accounts management, ISP
+      services, and software development. With meticulous attention to detail,
+      we optimize financial processes, ensure seamless connectivity, and develop
+      bespoke software tailored to your needs. Trust Prime Technology for
+      unparalleled expertise and professionalism in every aspect of IT service
+      provision.
+    </p>
+
+    <h1 class="font-bold text-2xl mt-5 mb-2">Our Approach</h1>
+    <p>
+      At Prime Technology, we epitomize precision, innovation, and
+      client-centricity. We prioritize understanding your unique needs to craft
+      tailored solutions in accounts management, ISP services, and software
+      development. Through collaborative partnerships and cutting-edge
+      technology, we ensure unparalleled efficiency, reliability, and
+      satisfaction in every project we undertake.
+    </p>
+
+    <h1 class="font-bold text-2xl mt-5 mb-2">Our Team</h1>
+    <p>
+      Our Team reflects a synergy of expertise and dedication. Comprising
+      skilled professionals across accounts management, ISP services, and
+      software development, our team is committed to delivering excellence.
+      Through seamless collaboration and continual refinement, we ensure
+      innovative solutions that exceed client expectations, driving success and
+      growth.
+    </p>
+  </div>
+  <div class="w-1/2 flex justify-center aboutImage">
+    <img src={aboutImage} alt="aboutImage" class=" p-10" />
+  </div>
+</div>
+
+<style>
+  @media only screen and (max-width: 639px) {
+    .aboutContainer {
+      flex-direction: column;
+    }
+
+    .aboutText {
+      width: 100%;
+      text-align: center;
+    }
+
+    .aboutImage {
+      width: 100%;
+    }
+  }
+
+  @media only screen and (max-width: 900px) {
+    .aboutContainer {
+      flex-direction: column;
+    }
+
+    .aboutText {
+      width: 100%;
+      text-align: center;
+    }
+
+    .aboutImage {
+      width: 100%;
+    }
+  }
+</style>
